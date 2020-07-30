bbedf4dce32d100645066da311b0b30076878370
Mohamad El Bohsaly <mohamad.elbohsaly@gmail.com>
2020-07-30 16:03:55 +0300

    Create DeleteCard requirement

--- team5-payment/team5-payment/Data/Card.cs
+++ team5-payment/team5-payment/Data/Card.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using Moneris;
+
+
 namespace team5_payment.Data
 {
-    public class EmptyClass
+    public class Card
     {
-        public EmptyClass()
+        private string firstName;
+        private string lastName;
+        private string cardNumber;
+        public string maskCard;
+        public int cvvNumber;
+        public string expiryDate;
+
+
+        public Card(string firstName, string lastName, string cardNumber, int cvvNumber, string expiryDate)
         {
+            this.cardNumber = cardNumber;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.cvvNumber = cvvNumber;
+            this.expiryDate = expiryDate;
+            this.maskCard = (new HttpsPostRequest()).GetReceipt().GetMaskedPan();
         }
     }
 }
