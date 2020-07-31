cce9f84003a8372713fc1720a416da63964103df
Mohamad El Bohsaly <mohamad.elbohsaly@gmail.com>
2020-07-31 17:16:50 +0300

    Tweak HttpPost

--- team5-payment/team5-payment/Controllers/PaymentsController.cs
+++ team5-payment/team5-payment/Controllers/PaymentsController.cs
@@ -5,13 +5,49 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moneris;
+using System.Net.Http;
+using System.Text;
+using System.Collections;
+using team5_payment.Data;
 
 namespace team5_payment.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
     public class PaymentsController : ControllerBase
     {
         Completion completion = new Completion();
+        private static readonly HttpClient client = new HttpClient();
+        private static Dictionary<string, string> response = new Dictionary<string, string>();
+        
+        private readonly IPayment _repository;
+
+        public PaymentsController(IPayment repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost("delete")]
+        public ActionResult<Dictionary<string,string>> DeleteCard(Card card)
+        {
+            try
+            {   
+                response = _repository.deleteCard(card);
+            }
+            catch (Exception exception)
+            {
+                response = new Dictionary<string, string>
+                {
+                    {"Card ID", $"{exception.Message}"},
+                    {"Valut Success", $"{exception.Message}" }
+                };
+
+            }
+
+            return Ok(response);
+        }
     }
+
+
 }
---
+++ team5-payment/team5-payment/Data/MockPayment.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Moneris;
+
+namespace team5_payment.Data
+{
+    public class MockPayment : IPayment
+    {
+        public MockPayment()
+        {
+        }
+
+        public static HttpsPostRequest postRequest = new HttpsPostRequest();
+
+        public Dictionary<string, string> deleteCard(Card card)
+        {
+            Receipt receipt = postRequest.GetReceipt();
+
+            ResDelete resDelete = new ResDelete(receipt.GetDataKey());
+
+            string store_id = "store5";
+            string api_token = "yesguy";
+            string data_key = "PjVKjtEmc1FvFyjxHE4EwBMxi";
+
+            ResLookupMasked resLookupMasked = new ResLookupMasked();
+            resLookupMasked.SetDataKey(data_key);
+            card.cardID = data_key;
+
+            string processing_country_code = "CA";
+            bool status_check = false;
+
+            postRequest.SetProcCountryCode(processing_country_code);
+            postRequest.SetTestMode(true); //false or comment out this line for production transactions
+            postRequest.SetStoreId(store_id);
+            postRequest.SetApiToken(api_token);
+            postRequest.SetTransaction(resDelete);
+            postRequest.SetStatusCheck(status_check);
+            postRequest.Send();
+            postRequest.SetTransaction(resDelete);
+                    
+
+            var result = new Dictionary<string, string>
+            {
+                {"Card ID", $"{data_key}" },
+                {"Vault Success", $"{card.cardID}" }
+            };
+
+            return result;
+        }
+
+        
+    }
+}
