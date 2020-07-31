05c1953701d7e2f31f02f7a91ceab5b777f64d96
tonyhabib01 <34315675+tonyhabib01@users.noreply.github.com>
2020-08-01 00:09:14 +0300

    fix vault purchase

--- team5-payment/team5-payment/Models/PaymentProccess.cs
+++ team5-payment/team5-payment/Models/PaymentProccess.cs
@@ -8,8 +8,8 @@ namespace team5_payment.Models
     public class PaymentProccess
     {
         public string OrderId { get; set; }
+        public string DataKey { get; set; }
         public string Amount { get; set; }
-        public string Pan { get; set; }
         public string ExpDate { get; set; }
     }
 }
