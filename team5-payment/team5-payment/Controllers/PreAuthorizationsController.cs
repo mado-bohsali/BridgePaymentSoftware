1264810b31da34d72d576f46988140c04a7aa5aa
tonyhabib01 <34315675+tonyhabib01@users.noreply.github.com>
2020-08-02 23:55:18 +0300

    Add PreAuth

---
+++ team5-payment/team5-payment/Controllers/PreAuthorizationsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using team5_payment.Data;
+using team5_payment.Models;
+
+namespace team5_payment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PreAuthorizationsController : ControllerBase
+    {
+        private readonly IPreAuthorization _repository;
+
+        public PreAuthorizationsController (IPreAuthorization repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost("Vault")]
+        public ActionResult PreAuthWithVault(PaymentProccess paymentProcess)
+        {
+            try
+            {
+                dynamic response = _repository.PreAuthWithVault(paymentProcess);
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ team5-payment/team5-payment/Data/IPreAuthorization.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using team5_payment.Models;
+
+namespace team5_payment.Data
+{
+    public interface IPreAuthorization
+    {
+        public object PreAuthWithVault(PaymentProccess paymentProcess);
+    }
+}
