01210354d88b113e08cf8e1e2aef8a7c01a223b1
Mira Adra <myra7314@gmail.com>
2020-08-07 00:15:53 +0300

    testt

---
+++ team5-payment/team5-payment/Controllers/PaymentsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moneris;
+
+namespace team5_payment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        Completion completion = new Completion();
+    }
+}
